afe2a6d089528db311a7de07b557d1cc862e396e
Nathan Lopes <nathan.qualitysp@gmail.com>
2023-12-19 10:35:01 -0300

    Improve database,js

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -7,12 +7,14 @@ async function status(request, response) {
     const dataBaseVersionResult = await database.query("SHOW server_version;");
     const dbVersion = dataBaseVersionResult.rows[0].server_version;
 
+    const databaseName = process.env.POSTGRES_DB;
     const showMaxConnections = await database.query("SHOW max_connections;");
     const maxConnections = showMaxConnections.rows[0].max_connections;
 
-    const showOpenedConnections = await database.query(
-      "SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db';",
-    );
+    const showOpenedConnections = await database.query({
+      text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+      values: [databaseName],
+    });
     const currentConnections = showOpenedConnections.rows[0].count;
 
     response.status(200).json({
